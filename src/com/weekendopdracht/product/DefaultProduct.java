806266c0ac0c992686a98eeea4eafdeae1f97498
jellevdveen <jellevdveen16@gmail.com>
2016-03-19 17:31:24 +0100

    updated product and default product

--- src/com/weekendopdracht/product/DefaultProduct.java
+++ src/com/weekendopdracht/product/DefaultProduct.java
@@ -1,5 +1,22 @@
 package com.weekendopdracht.product;
 
-public class DefaultProduct {
+public class DefaultProduct extends Product {
+	private String name;
+	private int stock;
+	
+	// getters
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getStock() {
+		return stock; 
+	}
 
+	// setters
+	
+	// perhaps default access would suffice
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
 }
--- src/com/weekendopdracht/product/Product.java
+++ src/com/weekendopdracht/product/Product.java
@@ -1,5 +1,18 @@
 package com.weekendopdracht.product;
 
 public abstract class Product {
-
+	public abstract String getName();
+	public abstract int getStock();
+	
+	
+	/* according to the assignment setStock should be defined here, but I don't see how, as Product class
+	does not have any instance variables according to the same assignment*/
+	public abstract void setStock(int stock);
+	
+	public void takeFromStock(int amount) {
+		setStock(getStock() - amount);
+	}
+	
+	
+		
 }
