be6d0ed87f60b3afe2a3e28e3e6b40a837531aa2
jellevdveen <jellevdveen16@gmail.com>
2016-03-19 18:41:23 +0100

    made some changes, now looks nicer

--- src/com/weekendopdracht/main/Main.java
+++ src/com/weekendopdracht/main/Main.java
@@ -12,24 +12,46 @@ public class Main {
 		Stock.addTo("Appels", 10, "Default");
 		Stock.addTo("Bananen", 10, "Default");
 		
-		printFormattedList(30);
-		
-		
 		Stock.addTo("Appels", 10, "Default");
+		Stock.addTo("Altviolen", 10, "Default");
 		
-		printFormattedList(30);
+		printFormattedStockList(30);
 
 	}
 	
-	private static void printFormattedList(int lengthOfProductName) {
-		for (Product p : Stock.getFullStock()) {
-			String formattedName = p.getName();
+	// function that shows/will show the full stock in an eye-pleasing way
+	private static void printFormattedStockList(int lengthOfProductName) {
+		drawLine(lengthOfProductName + 11);
 		
-				for (int i = 0; i + p.getName().length() < lengthOfProductName; i++) {
-					formattedName = formattedName.concat(" ");
-				}
-				System.out.println(formattedName + p.getStock());
+		String tempString = "Productnaam";
+		
+		for (int i = 0; i + 11 < lengthOfProductName; i++) {
+			tempString = tempString.concat(" ");
 		}
+		System.out.println(tempString + "Hoeveelheid");
+		
+		
+		drawLine(lengthOfProductName + 11);
+		
+		
+		for (Product p : Stock.getFullStock()) {
+			tempString = p.getName();
+		
+			for (int i = 0; i + p.getName().length() < lengthOfProductName; i++) {
+				tempString = tempString.concat(" ");
+			}
+			System.out.println(tempString + p.getStock());
+		}
+		
+		drawLine(lengthOfProductName + 11);
+	}
+	
+	private static void drawLine(int length){
+		String tempString = "";
+		for (int i = 0; i < length; i++) {
+			tempString = tempString.concat("-");
+		}
+		System.out.println(tempString);
 	}
 
 }
